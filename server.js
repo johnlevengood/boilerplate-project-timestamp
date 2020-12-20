34e856c8cf5f3885d2d518c06866c124e3421c1e
johnlevengood <50719877+johnlevengood@users.noreply.github.com>
2020-12-20 13:14:04 -0500

    main start

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
 
 // init project
 var express = require('express');
+const moment = require('moment')
+const PORT = process.env.port || 3000
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+app.use(express.json())
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +25,37 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.send({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp', (req,res) => {
+    const now = moment.utc()
+    const resp = {
+        unix: now.valueOf(),
+        utc: now.format('ddd, D MMM YYYY HH:mm:ss') + ' GMT'
+    }
+    return res.status(200).send(resp)
+})
 
+app.get('/api/timestamp/:date', (req,res) => {
+    let date = req.params.date
+    if (isNaN(date)){
+        date = moment.utc(new Date(req.params.date))
+    } else {
+        date = moment.utc(Number(date))
+    }
+    if (date.isValid() === false){
+        return res.status(400).send({error:'Invalid Date'})
+    }
+    const resp = {
+        unix: date.valueOf(),
+        utc: date.format('ddd, DD MMM YYYY HH:mm:ss') + ' GMT'
+    }
+    return res.status(200).send(resp)
+
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + PORT);
 });
